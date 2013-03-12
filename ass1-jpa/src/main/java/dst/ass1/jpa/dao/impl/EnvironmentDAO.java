e9a8957f5a18f5520847c638d9b8609701dbe89e
Jakob Gruber <jakob.gruber@gmail.com>
2013-03-12 13:07:34 +0100

    Implement a generic JPA DAO and convert EnvironmentDAO

--- ass1-jpa/src/main/java/dst/ass1/jpa/dao/impl/EnvironmentDAO.java
+++ ass1-jpa/src/main/java/dst/ass1/jpa/dao/impl/EnvironmentDAO.java
@@ -1,44 +1,19 @@
 package dst.ass1.jpa.dao.impl;
 
-import java.util.List;
-
 import org.hibernate.Session;
 
 import dst.ass1.jpa.dao.IEnvironmentDAO;
 import dst.ass1.jpa.model.IEnvironment;
 
-public class EnvironmentDAO implements IEnvironmentDAO {
-
-    private final Session session;
+public class EnvironmentDAO extends GenericJpaDAO<IEnvironment> implements IEnvironmentDAO {
 
     public EnvironmentDAO(Session session) {
-        this.session = session;
+        super(session);
     }
 
     @Override
-    public IEnvironment findById(Long id) {
-        List<IEnvironment> l = null;
-
-        session.beginTransaction();
-        l = session.createQuery("from Environment where id = :id").setParameter("id", id).list();
-        session.getTransaction().commit();
-
-        if (l.isEmpty()) {
-            return null;
-        }
-
-        return l.get(0);
-    }
-
-    @Override
-    public List<IEnvironment> findAll() {
-        List<IEnvironment> l = null;
-
-        session.beginTransaction();
-        l = session.createQuery("from Environment").list();
-        session.getTransaction().commit();
-
-        return l;
+    protected String getTableName() {
+        return "Environment";
     }
 
 }
---
+++ ass1-jpa/src/main/java/dst/ass1/jpa/dao/impl/GenericJpaDAO.java
@@ -0,0 +1,44 @@
+package dst.ass1.jpa.dao.impl;
+
+import java.util.List;
+
+import org.hibernate.Session;
+
+import dst.ass1.jpa.dao.GenericDAO;
+
+public abstract class GenericJpaDAO<T> implements GenericDAO<T> {
+
+    private final Session session;
+
+    public GenericJpaDAO(Session session) {
+        this.session = session;
+    }
+
+    @Override
+    public T findById(Long id) {
+        String query = String.format("from %s where %s = :id",
+                getTableName(),
+                getIdColumnName());
+
+        List<T> l = session.createQuery(query).setParameter("id", id).list();
+
+        if (l.isEmpty()) {
+            return null;
+        }
+
+        return l.get(0);
+    }
+
+    @Override
+    public List<T> findAll() {
+        String query = String.format("from %s", getTableName());
+        return session.createQuery(query).list();
+    }
+
+    protected abstract String getTableName();
+
+    protected String getIdColumnName() {
+        return "id";
+    }
+
+}
