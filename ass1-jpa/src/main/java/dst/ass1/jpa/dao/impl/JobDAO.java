b19e04074aa8c8a974aa5ec2145bce428d2b6820
Jakob Gruber <jakob.gruber@gmail.com>
2013-03-14 19:09:09 +0100

    Job criteria queries

--- ass1-jpa/src/main/java/dst/ass1/jpa/dao/impl/JobDAO.java
+++ ass1-jpa/src/main/java/dst/ass1/jpa/dao/impl/JobDAO.java
@@ -3,10 +3,15 @@ package dst.ass1.jpa.dao.impl;
 import java.util.Date;
 import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Example;
+import org.hibernate.criterion.Restrictions;
 
 import dst.ass1.jpa.dao.IJobDAO;
 import dst.ass1.jpa.model.IJob;
+import dst.ass1.jpa.model.JobStatus;
+import dst.ass1.jpa.model.impl.Execution;
 import dst.ass1.jpa.model.impl.Job;
 
 public class JobDAO extends GenericJpaDAO<IJob> implements IJobDAO {
@@ -17,14 +22,43 @@ public class JobDAO extends GenericJpaDAO<IJob> implements IJobDAO {
 
     @Override
     public List<IJob> findJobsForUserAndWorkflow(String user, String workflow) {
-        // TODO Auto-generated method stub
-        return null;
+        Criteria c = getSession().createCriteria(Job.class)
+                .createAlias("user", "u")
+                .createAlias("environment", "e");
+
+        if (user != null && !user.isEmpty()) {
+            c.add(Restrictions.eq("u.username", user));
+        }
+
+        if (workflow != null && !workflow.isEmpty()) {
+            c.add(Restrictions.eq("e.workflow", workflow));
+        }
+
+        @SuppressWarnings("unchecked")
+        List<IJob> results = c.list();
+
+        return results;
     }
 
     @Override
     public List<IJob> findJobForStatusFinishedStartandFinish(Date start, Date finish) {
-        // TODO Auto-generated method stub
-        return null;
+        Execution execution = new Execution();
+        execution.setStatus(JobStatus.FINISHED);
+        execution.setStart(start);
+        execution.setEnd(finish);
+
+        Job job = new Job();
+        job.setExecution(execution);
+
+        Criteria c = getSession().createCriteria(Job.class)
+                .add(Example.create(job))
+                .createCriteria("execution")
+                .add(Example.create(job.getExecution()));
+
+        @SuppressWarnings("unchecked")
+        List<IJob> results = c.list();
+
+        return results;
     }
 
     @Override
