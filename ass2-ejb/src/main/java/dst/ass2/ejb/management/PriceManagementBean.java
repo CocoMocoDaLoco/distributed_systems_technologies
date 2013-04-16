0ce2c0dc2a950f42afc566ead0a09c608f0800bd
Jakob Gruber <jakob.gruber@gmail.com>
2013-04-16 14:50:10 +0200

    Implement Price

--- ass2-ejb/src/main/java/dst/ass2/ejb/management/PriceManagementBean.java
+++ ass2-ejb/src/main/java/dst/ass2/ejb/management/PriceManagementBean.java
@@ -8,6 +8,7 @@ import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
 import dst.ass2.ejb.management.interfaces.IPriceManagementBean;
+import dst.ass2.ejb.model.IPrice;
 
 /* Singleton was chosen because we have a central state
  * which needs to be initialized at startup. Both stateless and stateful
@@ -18,28 +19,64 @@ import dst.ass2.ejb.management.interfaces.IPriceManagementBean;
 @Singleton
 @Startup
 public class PriceManagementBean implements IPriceManagementBean {
-	
-	// TODO
 
     @PostConstruct
     public void postConstruct() {
         /* TODO: Load the data. */
     }
-	
-	@Override
-	public BigDecimal getPrice(Integer nrOfHistoricalJobs) {
-		// TODO
-		return new BigDecimal(0.0);
-	}
 
-	@Override
-	public void setPrice(Integer nrOfHistoricalJobs, BigDecimal price) {
-		// TODO
-	}
-	
-	
-	@Override
-	public void clearCache() {
-		// TODO
-	}
+    @Override
+    public BigDecimal getPrice(Integer nrOfHistoricalJobs) {
+        // TODO
+        return new BigDecimal(0.0);
+    }
+
+    @Override
+    public void setPrice(Integer nrOfHistoricalJobs, BigDecimal price) {
+        // TODO
+    }
+
+
+    @Override
+    public void clearCache() {
+        // TODO
+    }
+
+    private static class Price implements IPrice {
+
+        private long id;
+        private int nrOfHistoricalJobs;
+        private BigDecimal price;
+
+        @Override
+        public Long getId() {
+            return id;
+        }
+
+        @Override
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        @Override
+        public Integer getNrOfHistoricalJobs() {
+            return nrOfHistoricalJobs;
+        }
+
+        @Override
+        public void setNrOfHistoricalJobs(Integer nrOfHistoricalJobs) {
+            this.nrOfHistoricalJobs = nrOfHistoricalJobs;
+        }
+
+        @Override
+        public BigDecimal getPrice() {
+            return price;
+        }
+
+        @Override
+        public void setPrice(BigDecimal price) {
+            this.price = price;
+        }
+
+    }
 }
