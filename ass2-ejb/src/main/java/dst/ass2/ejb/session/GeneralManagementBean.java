bd021402e1932673437d3e7525d99d25de204bf0
Jakob Gruber <jakob.gruber@gmail.com>
2013-04-29 15:18:51 +0200

    Bill calculation

--- ass2-ejb/src/main/java/dst/ass2/ejb/session/GeneralManagementBean.java
+++ ass2-ejb/src/main/java/dst/ass2/ejb/session/GeneralManagementBean.java
@@ -1,6 +1,8 @@
 package dst.ass2.ejb.session;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.Future;
 
@@ -14,9 +16,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
+import dst.ass1.jpa.model.IComputer;
+import dst.ass1.jpa.model.IGrid;
+import dst.ass1.jpa.model.IJob;
+import dst.ass1.jpa.model.IMembership;
 import dst.ass1.jpa.model.IUser;
 import dst.ass2.ejb.dto.AuditLogDTO;
 import dst.ass2.ejb.dto.BillDTO;
+import dst.ass2.ejb.dto.BillDTO.BillPerJob;
 import dst.ass2.ejb.management.PriceManagementBean;
 import dst.ass2.ejb.management.interfaces.IPriceManagementBean;
 import dst.ass2.ejb.model.IAuditLog;
@@ -49,6 +56,9 @@ public class GeneralManagementBean implements IGeneralManagementBean {
     @Asynchronous
     public Future<BillDTO> getBillForUser(String username) throws Exception {
         BillDTO bill = new BillDTO();
+        bill.setBills(new LinkedList<BillDTO.BillPerJob>());
+        bill.setUsername(username);
+        bill.setTotalPrice(new BigDecimal(0));
 
         IUser user = null;
         try {
@@ -60,7 +70,61 @@ public class GeneralManagementBean implements IGeneralManagementBean {
             throw new AssignmentException("Could not find specified user", e);
         }
 
+        List<IJob> unpaidJobs = entityManager
+                .createQuery("from Job j where j.user.username = :username and isPaid = false", IJob.class)
+                .setParameter("username", username)
+                .getResultList();
+
+        final int totalJobCount = entityManager
+                .createQuery("from Job j where j.user.username = :username", IJob.class)
+                .setParameter("username", username)
+                .getResultList().size();
+
+        int paidJobs = totalJobCount - unpaidJobs.size();
+
+        for (IJob job : unpaidJobs) {
+            billJob(paidJobs++, bill, job, user);
+        }
+
+        entityManager.flush();
+
         return new AsyncResult<BillDTO>(bill);
+    }
+
+    private void billJob(int paidJobs, BillDTO bill, IJob job, IUser user) throws AssignmentException {
+        final List<IComputer> computers = job.getExecution().getComputers();
+        if (computers.isEmpty()) {
+            System.out.printf("Job not assigned to any computers%n");
+            throw new AssignmentException("Job not assigned to any computers");
+        }
+
+        final IGrid grid = computers.get(0).getCluster().getGrid();
+
+        List<IMembership> memberships = entityManager
+                .createQuery("select m from User u join u.memberships m join m.grid g " +
+                        "where u.username = :username and g.id = :gridid", IMembership.class)
+                        .setParameter("username", user.getUsername())
+                        .setParameter("gridid", grid.getId())
+                        .getResultList();
+
+        final BigDecimal discount = (memberships.size() > 0) ? new BigDecimal(memberships.get(0).getDiscount()) : new BigDecimal(0);
+        final BigDecimal fraction = new BigDecimal(1).subtract(discount);
+        final BigDecimal cs = priceManagementBean.getPrice(paidJobs).multiply(fraction);
+        final BigDecimal executionMinutes = new BigDecimal(job.getExecutionTime() / 60);
+        final BigDecimal ce = grid.getCostsPerCPUMinute().multiply(executionMinutes).multiply(fraction);
+
+        BillPerJob bpj = bill.new BillPerJob();
+        bpj.setJobId(job.getId());
+        bpj.setNumberOfComputers(job.getExecution().getComputers().size());
+        bpj.setSetupCosts(cs.setScale(2, RoundingMode.HALF_UP));
+        bpj.setExecutionCosts(ce.setScale(2, RoundingMode.HALF_UP));
+        bpj.setJobCosts(ce.add(cs).setScale(2, RoundingMode.HALF_UP));
+
+        bill.getBills().add(bpj);
+        bill.setTotalPrice(bill.getTotalPrice().add(bpj.getJobCosts()));
+
+        job.setPaid(true);
+        entityManager.persist(job);
     }
 
     @Override
