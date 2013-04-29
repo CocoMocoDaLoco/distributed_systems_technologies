b7c7653b742cc709fc1577a77bdafe504bbd91b0
Jakob Gruber <jakob.gruber@gmail.com>
2013-04-29 21:21:53 +0200

    Increment the @ComponentId for each class object

--- ass2-di/src/main/java/dst/ass2/di/impl/InjectionController.java
+++ ass2-di/src/main/java/dst/ass2/di/impl/InjectionController.java
@@ -14,19 +14,22 @@ public class InjectionController implements IInjectionController {
     private final HashMap<Class<?>, Object> singletons =
             new HashMap<Class<?>, Object>();
 
+    private final HashMap<Class<?>, Long> ids = new HashMap<Class<?>, Long>();
+
     @Override
     public void initialize(Object obj) throws InjectionException {
         final Class<?> clazz = obj.getClass();
         assertComponent(clazz);
 
         boolean idFound = false;
+        final Long id = getAndIncrementId(clazz);
 
         for (Field field : clazz.getDeclaredFields()) {
             final String fname = field.getName();
 
             for (Annotation annotation : field.getDeclaredAnnotations()) {
                 if (annotation.annotationType() == ComponentId.class) {
-                    injectId(obj, field);
+                    injectId(id, obj, field);
                     idFound = true;
                 }
             }
@@ -69,9 +72,28 @@ public class InjectionController implements IInjectionController {
         throw new InjectionException("Not a component");
     }
 
-    private void injectId(Object obj, Field field) {
+    private void injectId(Long id, Object obj, Field field) {
         if (field.getType() != Long.class) {
             throw new InjectionException("Id must be of type Long");
+        }
+
+        try {
+            field.set(obj, id);
+        } catch (IllegalArgumentException e) {
+            throw new InjectionException(e);
+        } catch (IllegalAccessException e) {
+            throw new InjectionException(e);
+        }
+    }
+
+    private Long getAndIncrementId(Class<?> clazz) {
+        synchronized (ids) {
+            Long id = ids.get(clazz);
+            if (id == null) {
+                id = 0L;
+            }
+            ids.put(clazz, id + 1);
+            return id;
         }
     }
 
