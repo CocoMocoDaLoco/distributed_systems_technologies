307427ed89c30569e8000e33476b2c455ffffae1
Jakob Gruber <jakob.gruber@gmail.com>
2013-04-29 13:56:34 +0200

    Throw if user does not exist in bill calculation

--- ass2-ejb/src/main/java/dst/ass2/ejb/session/GeneralManagementBean.java
+++ ass2-ejb/src/main/java/dst/ass2/ejb/session/GeneralManagementBean.java
@@ -4,15 +4,22 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.concurrent.Future;
 
+import javax.ejb.AsyncResult;
+import javax.ejb.Asynchronous;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
 
+import dst.ass1.jpa.model.IUser;
 import dst.ass2.ejb.dto.AuditLogDTO;
 import dst.ass2.ejb.dto.BillDTO;
 import dst.ass2.ejb.management.PriceManagementBean;
 import dst.ass2.ejb.management.interfaces.IPriceManagementBean;
+import dst.ass2.ejb.session.exception.AssignmentException;
 import dst.ass2.ejb.session.interfaces.IGeneralManagementBean;
 import dst.ass2.ejb.util.EJBUtils;
 
@@ -22,9 +29,11 @@ public class GeneralManagementBean implements IGeneralManagementBean {
 
     private final IPriceManagementBean priceManagementBean;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     public GeneralManagementBean() throws NamingException {
-        priceManagementBean = (IPriceManagementBean)
-                EJBUtils.lookup(new InitialContext(), PriceManagementBean.class);
+        priceManagementBean = EJBUtils.lookup(new InitialContext(), PriceManagementBean.class);
     }
 
     @Override
@@ -34,9 +43,21 @@ public class GeneralManagementBean implements IGeneralManagementBean {
 
 
     @Override
+    @Asynchronous
     public Future<BillDTO> getBillForUser(String username) throws Exception {
-        // TODO
-        return null;
+        BillDTO bill = new BillDTO();
+
+        IUser user = null;
+        try {
+            user = entityManager
+                    .createQuery("from User u where u.username = :username", IUser.class)
+                    .setParameter("username", username)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            throw new AssignmentException("Could not find specified user", e);
+        }
+
+        return new AsyncResult<BillDTO>(bill);
     }
 
     @Override
