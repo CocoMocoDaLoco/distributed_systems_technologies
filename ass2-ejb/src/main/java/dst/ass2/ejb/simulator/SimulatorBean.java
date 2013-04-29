95dc744749ad71ca1e1ccb87afadb5f8e3020f65
Jakob Gruber <jakob.gruber@gmail.com>
2013-04-29 11:44:53 +0200

    Initialize the simulator timer service

--- ass2-ejb/src/main/java/dst/ass2/ejb/simulator/SimulatorBean.java
+++ ass2-ejb/src/main/java/dst/ass2/ejb/simulator/SimulatorBean.java
@@ -1,9 +1,26 @@
 package dst.ass2.ejb.simulator;
 
+import javax.annotation.Resource;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ejb.TimerService;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Singleton
+@Startup
 public class SimulatorBean {
-    
+
+    @Resource
+    private TimerService timerService;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Schedule(hour="*", minute = "*", second = "*/15")
     public void simulate() {
-        //TODO
+        System.out.println("OMG");
     }
 
 }
