8446f11bcf19f427e2b2b80e24f01fd82ac1dd90
Jakob Gruber <jakob.gruber@gmail.com>
2013-04-29 12:21:57 +0200

    Mark jobs as finished in simulator bean

--- ass2-ejb/src/main/java/dst/ass2/ejb/simulator/SimulatorBean.java
+++ ass2-ejb/src/main/java/dst/ass2/ejb/simulator/SimulatorBean.java
@@ -1,5 +1,8 @@
 package dst.ass2.ejb.simulator;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
@@ -7,6 +10,11 @@ import javax.ejb.Startup;
 import javax.ejb.TimerService;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import dst.ass1.jpa.model.IExecution;
+import dst.ass1.jpa.model.IJob;
+import dst.ass1.jpa.model.JobStatus;
 
 @Singleton
 @Startup
@@ -20,7 +28,25 @@ public class SimulatorBean {
 
     @Schedule(hour="*", minute = "*", second = "*/15")
     public void simulate() {
-        System.out.println("OMG");
+        TypedQuery<IJob> q = entityManager.createQuery("select j from Job j join j.execution e where e.end is null", IJob.class);
+        List<IJob> jobs = q.getResultList();
+
+        final Date now = new Date();
+
+        for (IJob j : jobs) {
+            if (j.getExecution().getStart().before(now)) {
+                finish(j, now);
+            }
+        }
+    }
+
+    private void finish(IJob job, Date now) {
+        IExecution e = job.getExecution();
+        e.setStatus(JobStatus.FINISHED);
+        e.setEnd(now);
+        job.setExecutionTime((int)(now.getTime() - e.getStart().getTime()));
+
+        entityManager.persist(job);
     }
 
 }
