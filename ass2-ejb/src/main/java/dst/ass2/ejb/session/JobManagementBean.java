53d184f6c40e15317da7e6d9a9f6e2c4e57fcc5d
Jakob Gruber <jakob.gruber@gmail.com>
2013-04-29 08:55:46 +0200

    Job management: login

--- ass2-ejb/src/main/java/dst/ass2/ejb/session/JobManagementBean.java
+++ ass2-ejb/src/main/java/dst/ass2/ejb/session/JobManagementBean.java
@@ -1,22 +1,27 @@
 package dst.ass2.ejb.session;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 import javax.ejb.Remote;
-import javax.ejb.Singleton;
+import javax.ejb.Stateful;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import dst.ass2.ejb.dto.AssignmentDTO;
 import dst.ass2.ejb.session.exception.AssignmentException;
 import dst.ass2.ejb.session.interfaces.IJobManagementBean;
 
-/* TODO: Other bean type? */
+/* Stateful, because state is unique to a client/bean session. */
 
 @Remote(IJobManagementBean.class)
-@Singleton
+@Stateful
 public class JobManagementBean implements IJobManagementBean {
 
-    // TODO
-
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Override
     public void addJob(Long gridId, Integer numCPUs, String workflow,
@@ -28,7 +33,24 @@ public class JobManagementBean implements IJobManagementBean {
     @Override
     public void login(String username, String password)
             throws AssignmentException {
-        // TODO
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new AssignmentException(e);
+        }
+        byte[] b = md.digest(password.getBytes());
+
+        Query q = entityManager.createQuery("from User " +
+                "where username = :username and password = :password");
+        q.setParameter("username", username);
+        q.setParameter("password", b);
+
+        int count = q.getResultList().size();
+
+        if (count != 1) {
+            throw new AssignmentException("Incorrect user or password");
+        }
     }
 
     @Override
