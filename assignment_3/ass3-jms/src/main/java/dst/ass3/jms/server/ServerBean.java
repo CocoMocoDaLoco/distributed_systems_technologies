4f023a6e4cdbf2c4f6a112c564330e1a7474ab3b
Jakob Gruber <jakob.gruber@gmail.com>
2013-05-20 10:45:04 +0200

    Inject queues and topics into server bean

--- assignment_3/ass3-jms/src/main/java/dst/ass3/jms/server/ServerBean.java
+++ assignment_3/ass3-jms/src/main/java/dst/ass3/jms/server/ServerBean.java
@@ -1,15 +1,27 @@
 package dst.ass3.jms.server;
 
+import javax.annotation.Resource;
 import javax.ejb.MessageDriven;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.Queue;
 import javax.jms.TextMessage;
+import javax.jms.Topic;
 
 import dst.ass3.jms.Names;
 
 @MessageDriven(mappedName = Names.SERVER_QUEUE)
 public class ServerBean implements MessageListener {
+
+    @Resource(mappedName = Names.CLUSTER_QUEUE)
+    private Queue clusterQueue;
+
+    @Resource(mappedName = Names.SCHEDULER_QUEUE)
+    private Queue schedulerQueue;
+
+    @Resource(mappedName = Names.COMPUTER_TOPIC)
+    private Topic computerTopic;
 
     @Override
     public void onMessage(Message msg) {
