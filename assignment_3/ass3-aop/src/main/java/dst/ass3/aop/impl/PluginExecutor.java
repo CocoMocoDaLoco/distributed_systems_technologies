310d8534efa9ed97c522b75a77aa36c95eede1ec
Jakob Gruber <jakob.gruber@gmail.com>
2013-05-21 16:26:02 +0200

    Load and execute plugins when found

--- assignment_3/ass3-aop/src/main/java/dst/ass3/aop/impl/PluginExecutor.java
+++ assignment_3/ass3-aop/src/main/java/dst/ass3/aop/impl/PluginExecutor.java
@@ -1,14 +1,21 @@
 package dst.ass3.aop.impl;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Collections;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
+import dst.ass3.aop.IPluginExecutable;
 import dst.ass3.aop.IPluginExecutor;
 import dst.ass3.aop.impl.DirectoryMonitor.INewJarListener;
 
@@ -50,7 +57,52 @@ public class PluginExecutor implements IPluginExecutor, INewJarListener {
 
     @Override
     public void onNewJar(File file) {
-        System.out.printf("Found new JAR: %s%n", file.getName());
+        JarFile jarFile = null;
+        try {
+            System.out.printf("Found new JAR: %s%n", file.getName());
+
+            jarFile = new JarFile(file);
+            final Enumeration<JarEntry> e = jarFile.entries();
+
+            final URL[] urls = { new URL(String.format("jar:file:%s!/", file.getCanonicalPath())) };
+            final URLClassLoader cl = URLClassLoader.newInstance(urls);
+
+            while (e.hasMoreElements()) {
+                final JarEntry je = e.nextElement();
+                if(je.isDirectory() || !je.getName().endsWith(".class")){
+                    continue;
+                }
+
+                final String className = je.getName()
+                        .substring(0, je.getName().length() - ".class".length())
+                        .replace('/', '.');
+                final Class<?> c = cl.loadClass(className);
+
+                if (!IPluginExecutable.class.isAssignableFrom(c))  {
+                    continue;
+                }
+
+                final IPluginExecutable instance = (IPluginExecutable)c.newInstance();
+                executorService.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        instance.execute();
+
+                        /* TODO: After execution, free all resources? */
+                    }
+                });
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e1) {
+            e1.printStackTrace();
+        } catch (IllegalAccessException e1) {
+            e1.printStackTrace();
+        } finally {
+            try { if (jarFile != null) { jarFile.close(); } } catch (IOException e) { e.printStackTrace(); }
+        }
     }
 
 }
