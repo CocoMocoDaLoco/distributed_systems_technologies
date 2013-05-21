9f5a001ba44dd01b3859f5b445f32e815ef01e89
Jakob Gruber <jakob.gruber@gmail.com>
2013-05-21 13:20:07 +0200

    Set up (and shut down) event processing

--- assignment_3/ass3-event/src/main/java/dst/ass3/event/impl/EventProcessing.java
+++ assignment_3/ass3-event/src/main/java/dst/ass3/event/impl/EventProcessing.java
@@ -1,35 +1,63 @@
 package dst.ass3.event.impl;
 
+import com.espertech.esper.client.Configuration;
+import com.espertech.esper.client.EPAdministrator;
+import com.espertech.esper.client.EPRuntime;
+import com.espertech.esper.client.EPServiceProvider;
+import com.espertech.esper.client.EPServiceProviderManager;
 import com.espertech.esper.client.StatementAwareUpdateListener;
 
+import dst.ass3.dto.TaskDTO;
+import dst.ass3.event.Constants;
 import dst.ass3.event.IEventProcessing;
 import dst.ass3.model.ITask;
 
 public class EventProcessing implements IEventProcessing {
 
-    private StatementAwareUpdateListener listener;
+    private static final String PROVIDER_NAME = "EsperEngineDST";
+
+    private EPServiceProvider serviceProvider;
+    private EPRuntime runtime;
 
     @Override
     public void initializeAll(StatementAwareUpdateListener listener, boolean debug) {
-        this.listener = listener;
+        Configuration config = new Configuration();
+        config.addEventType(TaskDTO.class);
 
         if (debug) {
-            /* config.getEngineDefaults().getLogging().setEnableExecutionDebug(true);
-             * config.getEngineDefaults().getLogging().setEnableTimerDebug(false);
-             * config.getEngineDefaults().getLogging().setEnableQueryPlan(false); */
+            config.getEngineDefaults().getLogging().setEnableExecutionDebug(true);
+            config.getEngineDefaults().getLogging().setEnableTimerDebug(false);
+            config.getEngineDefaults().getLogging().setEnableQueryPlan(false);
         }
+
+        serviceProvider = EPServiceProviderManager.getProvider(PROVIDER_NAME, config);
+        runtime = serviceProvider.getEPRuntime();
+        EPAdministrator administrator = serviceProvider.getEPAdministrator();
+
+        String epl = String.format("create schema %s as (jobId long, timestamp long)",
+                Constants.EVENT_TASK_ASSIGNED);
+        administrator.createEPL(epl).addListener(listener);
+
+        epl = String.format("create schema %s as (jobId long, timestamp long)",
+                Constants.EVENT_TASK_PROCESSED);
+        administrator.createEPL(epl).addListener(listener);
+
+        epl = String.format("create schema %s as (jobId long, duration long)",
+                Constants.EVENT_TASK_DURATION);
+        administrator.createEPL(epl).addListener(listener);
     }
 
     @Override
     public void addEvent(ITask task) {
-        // TODO Auto-generated method stub
-
+        runtime.sendEvent(new TaskDTO(task));
     }
 
     @Override
     public void close() {
-        // TODO Auto-generated method stub
+        runtime = null;
 
+        serviceProvider.destroy();
+        serviceProvider = null;
     }
 
 }
